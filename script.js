f28808df18639d5b3c7156605fbf8fb06a94b15f
JuliaKotlyar <julia.kotlyar2014@gmail.com>
2021-03-31 17:38:34 +0300

    draw the clock face

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-let canvas = document.getElementById('canvas'); // created a canvas object
-let ctx = canvas.getContext('2D'); // create a 2D drawing object for the canvas object
+const canvas = document.getElementById('canvas'); // created a canvas object
+const ctx = canvas.getContext('2d'); // create a 2D drawing object for the canvas object
+let radius = canvas.height / 2; // calculate the clock radius
+ctx.translate(radius, radius); // position of the object (center)
+radius *= 0.9;
+
+// draw the clock face
+
+function drawClock() {
+    drawFace(ctx, radius);
+}
+
+function drawFace(ctx, radius) {
+    let grad;
+    ctx.beginPath(); // create outline
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI); // draw arc
+    ctx.fillStyle = 'white'; // white face background of the clock
+    ctx.fill();
+    grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
+    grad.addColorStop(0, '#333');
+    grad.addColorStop(0.5, 'white');
+    grad.addColorStop(1, '#333');
+    ctx.strokeStyle = grad;
+    ctx.lineWidth = radius * 0.1;
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+    ctx.fillStyle = '#333';
+    ctx.fill();
+}
+
+drawClock();
